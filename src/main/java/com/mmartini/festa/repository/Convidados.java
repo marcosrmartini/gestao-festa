d381e87b0c72d7f8a0554ee8fe91b3ff08ab8809
Marcos Martini <marcosrmartini@gmail.com>
2017-06-10 07:15:41 -0300

    Primeiro commit

---
+++ src/main/java/com/mmartini/festa/repository/Convidados.java
@@ -0,0 +1,9 @@
+package com.mmartini.festa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mmartini.festa.model.Convidado;
+
+public interface Convidados extends JpaRepository<Convidado, Long>{
+
+}
